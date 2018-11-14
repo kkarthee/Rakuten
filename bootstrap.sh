10a7651a0236116e0683dfa175c451503e226ad0
kkarthee <k.karthee@gmail.com>
2018-11-14 15:00:23 +0800

    .

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,8 @@ DEB_RELEASE=$(lsb_release -c -s)
       echo "Adding Puppet Labs repo for $DEB_RELEASE."
       apt-get install -y curl
      # curl  -O "http://apt.puppetlabs.com/puppetlabs-release-pc1-$DEB_RELEASE.deb"
-     Curl -O http://apt.puppetlabs.com/puppet5-release-$DEB_RELEASE.deb
+     curl -O http://apt.puppetlabs.com/puppet5-release-$DEB_RELEASE.deb
+     dpkg -i "puppet5-release-$DEB_RELEASE.deb"
       # dpkg -i "puppetlabs-release-pc1-$DEB_RELEASE.deb"
       #curl http://pm.puppetlabs.com/puppet-agent/2018.1.5/5.5.8/repos/puppet-agent-ubuntu-16.04-amd64.tar.gz -o /tmp/puppet-agent-ubuntu-16.04-amd64.tar.gz
      # gunzip /tmp/puppet-agent-ubuntu-16.04-amd64.tar.gz
